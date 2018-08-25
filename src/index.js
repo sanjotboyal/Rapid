58baa32ceee0f0f5067d913e6f589751e2fe601b
Jay C <jayc.chowdhary@gmail.com>
2018-08-25 11:35:59 -0400

    Added basic navigation functionality.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation';
 import { Tab } from './config/navigation';
+import { Home, Ledger, Login, Register, Room, RoomFeed } from './screens/';
+
+const RootStack = createStackNavigator(
+  {
+    Login: Login
+  },
+  {
+    initialRouteName: 'Login',
+    headerMode: 'none'
+  }
+);
+
 
 export default class App extends Component{
 	render() {
-		return ( <Tab /> );
+		return ( <RootStack /> );
 	}
 }
