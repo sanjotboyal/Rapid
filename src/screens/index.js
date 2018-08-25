f7b9e0cc3e85ba73afe339005b034481929d1546
Sanjot Boyal <sboyal2@uwo.ca>
2018-08-25 02:02:26 -0400

    Screen Indexing

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+export HomeScreen from './Home'
+export LedgerScreen from './Ledger'
+export LoginScreen from './Login'
+export RegisterScreen from './Register'
+export RoomScreen from './Room'
+export RoomFeed from './RoomFeed'
+
